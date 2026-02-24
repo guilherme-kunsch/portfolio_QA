82139ab1d8ea68555a33ff4b594d2568db3c6f31
guilherme-kunsch <guilherme.kunsch@outlook.com>
2026-02-24 17:27:25 -0300

    iniciando o projeto

---
+++ cypress/support/locators.js
@@ -0,0 +1,11 @@
+const locators = {
+  LOGIN: {
+    USER: '[name="username"]',
+    PASSWORD: '[name="password"]',
+    BTN_LOGIN: ".oxd-button",
+  },
+  URL: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+  MESSAGE: ".oxd-alert",
+};
+
+export default locators;
