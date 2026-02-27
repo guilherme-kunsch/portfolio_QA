3a19338c8ce6f9c0d7160df1537052168e74239c
guilherme-kunsch <guilherme.kunsch@outlook.com>
2026-02-26 22:48:20 -0300

    adicionando locators

--- cypress/e2e/recruitment.cy.js
+++ cypress/e2e/recruitment.cy.js
@@ -1,69 +1,70 @@
+import loc from "../support/locators";
 describe("Testing the application", () => {
-  // before(() => {
-  // })
-  it("Registering a user", () => {
+  it("Registering a recruitment", () => {
     cy.login("Admin", "admin123");
-    cy.contains(".oxd-main-menu-item", "Recruitment", { timeout: 10000 })
+    cy.contains(loc.RECRUITMENT.NAV, "Recruitment", { timeout: 10000 })
       .should("be.visible")
       .click();
 
-    cy.get(".orangehrm-header-container > .oxd-button", {
+    cy.get(loc.RECRUITMENT.ADD, {
       timeout: 10000,
     }).click();
 
-    cy.get('[name="firstName"]')
+    cy.get(loc.RECRUITMENT.FIRST_NAME)
       .type("Guilherme")
       .should("have.value", "Guilherme");
-    cy.get('[name="middleName"]').type("Kunsch").should("have.value", "Kunsch");
-    cy.get('[name="lastName"]')
+    cy.get(loc.RECRUITMENT.MIDDLE_NAME)
+      .type("Kunsch")
+      .should("have.value", "Kunsch");
+    cy.get(loc.RECRUITMENT.LAST_NAME)
       .type("Das Neves")
       .should("have.value", "Das Neves");
 
-    cy.get(".oxd-select-text").click();
-    cy.get(".oxd-select-dropdown")
+    cy.get(loc.RECRUITMENT.VACANCY).click();
+    cy.get(loc.RECRUITMENT.SELECT_VACANCY)
       .contains("Senior Support Specialist")
       .click();
 
-    cy.get(".oxd-grid-3.orangehrm-full-width-grid")
+    cy.get(loc.RECRUITMENT.EMAIL)
       .find("input")
       .eq(0)
       .type("teste@teste.com")
       .should("have.value", "teste@teste.com");
 
-    cy.get(".oxd-grid-3.orangehrm-full-width-grid")
+    cy.get(loc.RECRUITMENT.CONTACT_NUMBER)
       .find("input")
       .eq(1)
       .type("27997523768")
       .should("have.value", "27997523768");
 
-    cy.get(".oxd-file-input").selectFile(
+    cy.get(loc.RECRUITMENT.UPLOAD).selectFile(
       "cypress/fixtures/images/curriculo.pdf",
       { force: true },
     );
 
     cy.contains("curriculo.pdf").should("be.visible");
 
-    cy.get(".oxd-grid-3.orangehrm-full-width-grid")
+    cy.get(loc.RECRUITMENT.KEYWORDS)
       .find("input")
       .eq(3)
       .type("works, recruitment, emplooyes")
       .should("have.value", "works, recruitment, emplooyes");
 
-    cy.get(".oxd-date-wrapper").find("input").click();
-    cy.get(".oxd-calendar-wrapper")
+    cy.get(loc.RECRUITMENT.CALENDAR).find("input").click();
+    cy.get(loc.RECRUITMENT.SELECT_DATE)
       .contains(".oxd-calendar-date", "25")
       .click();
 
-    cy.get(".oxd-grid-3.orangehrm-full-width-grid")
+    cy.get(loc.RECRUITMENT.NOTES)
       .find("textarea")
       .type("Sabe programar e nadar!")
       .should("have.value", "Sabe programar e nadar!");
 
-    cy.get(".oxd-icon.bi-check.oxd-checkbox-input-icon").click();
+    cy.get(loc.RECRUITMENT.CHECKBOX).click();
 
-    cy.get(".oxd-button--secondary").click();
+    cy.get(loc.RECRUITMENT.SALVE_BUTTON).click();
 
-    cy.get(".oxd-toast", { timeout: 5000 }).should(
+    cy.get(loc.RECRUITMENT.MESSAGE, { timeout: 5000 }).should(
       "contain",
       "Successfully Save",
     );
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -7,6 +7,25 @@ const locators = {
   URL: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
   MESSAGE: ".oxd-alert",
   MESSAGE_REQUIRED: ".oxd-input-group > .oxd-text",
+  RECRUITMENT: {
+    NAV: ".oxd-main-menu-item",
+    ADD: ".orangehrm-header-container > .oxd-button",
+    FIRST_NAME: '[name="firstName"]',
+    MIDDLE_NAME: '[name="middleName"]',
+    LAST_NAME: '[name="lastName"]',
+    VACANCY: ".oxd-select-text",
+    SELECT_VACANCY: ".oxd-select-dropdown",
+    EMAIL: ".oxd-grid-3.orangehrm-full-width-grid",
+    CONTACT_NUMBER: ".oxd-grid-3.orangehrm-full-width-grid",
+    UPLOAD: ".oxd-file-input",
+    KEYWORDS: ".oxd-grid-3.orangehrm-full-width-grid",
+    CALENDAR: ".oxd-date-wrapper",
+    SELECT_DATE: ".oxd-calendar-date",
+    NOTES: ".oxd-grid-3.orangehrm-full-width-grid",
+    CHECKBOX: ".oxd-icon.bi-check.oxd-checkbox-input-icon",
+    SALVE_BUTTON: ".oxd-button--secondary",
+    MESSAGE: ".oxd-toast",
+  },
 };
 
 export default locators;
